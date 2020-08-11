883819c74161d6b483739e6d2bb7ea03952863b9
Crew Spence <csspence123@gmail.com>
2020-08-11 06:34:21 -0500

    Created function stub for toy problem.

---
+++ digit_count_radix_helper_udemy.js
@@ -0,0 +1,10 @@
+/*
+JavaScript Algorithms and Data Structures Masterclass
+digitCount and maxDigitCount
+
+digitCount(num) returns the number of digits in num.
+*/
+
+const digitCount = (num) => {
+  
+}
